b6f2bd497ee17d086dd973137595040f4e0016a7
tathagata <tathagatadg@gmail.com>
2016-04-17 20:04:50 -0500

    small fixes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-$SOURCE_PATH=~/Downloads/DonationProfiles #replace to match yours
-$DESTINATION_PATH=t@10.0.1.16:/home/t/projects/asha/ashadonations/DonationProfiles #replace to match yours
+#replace to match yours
+SOURCE_PATH=~/Downloads/DonationProfiles
+DESTINATION_PATH=t@10.0.1.16:/home/t/projects/asha/ashadonations/DonationProfiles
 rsync -avzheR --dry-run -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" $SOURCE_PATH $DESTINATION_PATH
